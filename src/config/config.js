548c940a8657af5cb4ace9c500c8ce7a198f4716
fixpix45 <37987154+fixpix45@users.noreply.github.com>
2020-07-15 11:47:32 -0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,11 @@
 export default {
-  authProvider: "google",
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: "AIzaSyCt6iJXEEflQxEOcqhMt5ul0fYNTtMxSd0",
+    authDomain: "octopus-9ee2f.firebaseapp.com",
+    databaseURL: "https://octopus-9ee2f.firebaseio.com",
+    projectId: "octopus-9ee2f",
+    storageBucket: "octopus-9ee2f.appspot.com",
+    messagingSenderId: "795223383400",
+    appId: "1:795223383400:web:147c4a054ac718d6a48add"
+
+
 };
